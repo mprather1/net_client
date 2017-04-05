4ff1181eae75c2400e2d46fd87cad47cf8daaa24
Michael Prather <mprather1@gmail.com>
2017-04-05 03:40:29 +0000

    socket

--- src/client.js
+++ src/client.js
@@ -1,16 +1,29 @@
 import net from 'net'
 import DuplexEmitter from 'duplex-emitter'
+import logger from 'winston-color'
+import chalk from 'chalk'
 
-const server = net.createServer()
+const s = new net.Socket()
 const port = process.env.PORT || 8000
 const environment = process.env.NODE_ENV || 'development'
 const hostname = process.env.HOSTNAME
 
-const socket = net.connect(port, hostname)
+const socket = s.connect(port, hostname)
+
+socket.on('connect', () => {
+  logger.info(chalk.magenta('connected...'))
+})
+socket.on('close', handleClose)
 const remoteEmitter = DuplexEmitter(socket)
 
 remoteEmitter.on('msg', onMessage)
 
 function onMessage (msg) {
+  logger.info(chalk.yellow('message received...'))
   console.log(msg)
+  socket.destroy()
+}
+
+function handleClose () {
+  logger.info(chalk.cyan('connection closed...'))
 }
